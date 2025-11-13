53a16438f1078aa9350ecacf50446e1a824b4654
bhashana99 <bhashanachamodya99@gmail.com>
2025-11-13 13:37:07 +0530

    basic logic for submit form

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css',
 })
 export class TaskFormComponent {
 
+  taskForm : FormGroup;
+
+ constructor(private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: [''],
+      description: ['']  
+    });
+  }
+
+
+  onSubmit(){
+    if (this.taskForm.valid) {
+    console.log(this.taskForm.value);
+    }
+  }
 }
