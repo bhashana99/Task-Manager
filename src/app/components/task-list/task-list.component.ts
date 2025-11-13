8451e4fb78eb136033966ab3415d2ae5ffd5bf96
bhashana99 <bhashanachamodya99@gmail.com>
2025-11-13 14:33:02 +0530

    display set of task list

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css',
 })
 export class TaskListComponent {
+
+  constructor(private taskService : TaskService){
+
+  }
+
+  tasks = computed(() => this.taskService.tasks());
 
 }
