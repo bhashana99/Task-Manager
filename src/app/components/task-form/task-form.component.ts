492ed3402645e9e1076797658bf26ceade589e77
bhashana99 <bhashanachamodya99@gmail.com>
2025-11-13 13:47:10 +0530

    business logic for create task

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
 
 
 @Component({
@@ -13,7 +14,7 @@ export class TaskFormComponent {
 
   taskForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private taskService : TaskService) {
     this.taskForm = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(3)]],
       description: ['', [Validators.required, Validators.minLength(5)]]
@@ -28,10 +29,14 @@ export class TaskFormComponent {
     return this.taskForm.get('description');
   }
 
-  
+
   onSubmit() {
     if (this.taskForm.valid) {
       console.log(this.taskForm.value);
+
+      const task = this.taskForm.value;
+      this.taskService.createTask(task);
+      this.taskForm.reset();
     }
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Task } from '../models/task.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
   
+  tasks = signal<Task[]> ([]);
+
+  createTask(task : Task){
+    task.id = new Date().getTime();
+    this.tasks.update(oldValues => [...oldValues,task]);
+    this.saveToLocalStorage();
+  }
+
+  private saveToLocalStorage(){
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 }
